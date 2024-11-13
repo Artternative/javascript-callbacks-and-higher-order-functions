fb8d9a57e24d38f64ae440b94d2c05589a247dfc
Artter <kasinat3mjt@gmail.com>
2024-11-14 01:58:46 +0700

    Fill:ex1-ex2

--- ex-1.js
+++ ex-1.js
@@ -1,12 +1,18 @@
 // Exercise #1: For Each Function
 
-function forEach(array, operation) {
-  // Start coding here
-}
-
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
-
+function forEach(array,operation){
+  for(let i = 0 ; i < array.length ; i++){
+    operation(array[i],i);
+  }
+}
+function operation(item , index){
+  //  [index] = item + 5000;
+   return newEmployeeSalaries[index] = item + 5000;
+}
 // Using `forEach` function here
+forEach(employeeSalaries,operation);
 
-console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
+console.log(newEmployeeSalaries); 
+// [25005, 45000, 37000, 19500, 349000]
